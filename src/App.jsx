74a21d01e121a680daa286d47a7afed6da4362de
ArisLooi <arislooi@outlook.com>
2025-01-15 23:16:06 +0800

    configure and setup App and Header, remove unused image, install axios package

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./context/AuthProvider";
+
+
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <AuthProvider>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </AuthProvider>
   )
 }
