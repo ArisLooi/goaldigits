be88459de44e4e4a0616300034ad27434fef54bf
ArisLooi <arislooi@outlook.com>
2025-01-16 21:19:30 +0800

    Initiate register page and Error Page, import to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Header from "./components/Header";
 import LoginPage from "./app/login/page";
 import BudgetsPage from "./app/budgets/page";
 import TransactionsPage from "./app/transactions/page";
+import ErrorPage from "./app/ErrorPage"
+import RegisterPage from "./app/register/page"
 
 export default function App() {
   return (
@@ -11,8 +13,10 @@ export default function App() {
       <Header />
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/budgets" element={<BudgetsPage />} />
         <Route path="/transactions" element={<TransactionsPage />} />
+        <Route path="/404" element={<ErrorPage />} />
         <Route path="/*" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
