f01be7e3ec6d9d60f41d895b08c32ab1046fb79b
ArisLooi <arislooi@outlook.com>
2025-01-16 09:28:21 +0800

    set up global.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider";
-
+import Header from "./components/Header";
 
 
 export default function App() {
   return (
+
     <AuthProvider>
+      <Header />
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
